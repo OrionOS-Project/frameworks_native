590966649bc97afdc6d1443331aa2ef67621404a
rmp22 <195054967+rmp22@users.noreply.github.com>
2026-06-18 09:56:04 +0800

    libs: Adding fallback opt-in for media open gl strategy
    Change-Id: I9632452964402f39ce67befe8dd70fadfe39cfe8
    Signed-off-by: Ghosuto <clash.raja10@gmail.com>
    Signed-off-by: romiyusnandar <yusromi04@gmail.com>

--- libs/renderengine/include/renderengine/LayerSettings.h
+++ libs/renderengine/include/renderengine/LayerSettings.h
@@ -23,6 +23,7 @@
 #include <gui/DisplayLuts.h>
 #include <math/mat4.h>
 #include <math/vec3.h>
+#include <renderengine/AxRenderEngineTypes.h>
 #include <renderengine/ExternalTexture.h>
 #include <renderengine/PrintMatrix.h>
 #include <ui/BlurRegion.h>
@@ -40,6 +41,7 @@
 
 #include <cstdint>
 #include <iosfwd>
+#include <memory>
 
 namespace android {
 namespace renderengine {
@@ -71,6 +73,8 @@ struct Buffer {
     bool isOpaque = false;
 
     float maxLuminanceNits = 0.0;
+
+    AxBufferContentHint axContentHint = AxBufferContentHint::Unknown;
 };
 
 // Metadata describing the layer geometry.
@@ -182,7 +186,8 @@ static inline bool operator==(const Buffer& lhs, const Buffer& rhs) {
             lhs.useTextureFiltering == rhs.useTextureFiltering &&
             lhs.textureTransform == rhs.textureTransform &&
             lhs.usePremultipliedAlpha == rhs.usePremultipliedAlpha &&
-            lhs.isOpaque == rhs.isOpaque && lhs.maxLuminanceNits == rhs.maxLuminanceNits;
+            lhs.isOpaque == rhs.isOpaque && lhs.maxLuminanceNits == rhs.maxLuminanceNits &&
+            lhs.axContentHint == rhs.axContentHint;
 }
 
 static inline bool operator==(const Geometry& lhs, const Geometry& rhs) {
@@ -232,6 +237,7 @@ static inline void PrintTo(const Buffer& settings, ::std::ostream* os) {
     *os << "\n    .usePremultipliedAlpha = " << settings.usePremultipliedAlpha;
     *os << "\n    .isOpaque = " << settings.isOpaque;
     *os << "\n    .maxLuminanceNits = " << settings.maxLuminanceNits;
+    *os << "\n    .axContentHint = " << toString(settings.axContentHint);
     *os << "\n}";
 }
 
